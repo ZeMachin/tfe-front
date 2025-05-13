65abc82d6a6b1b20edce310f779f7254649404c6
Sinouhé <sinouhes@gmail.com>
2025-05-13 23:42:27 +0200

    feat: assigned task - partial

--- src/app/components/chores/tasks/my-tasks/my-tasks.component.ts
+++ src/app/components/chores/tasks/my-tasks/my-tasks.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FamilyMember } from '../../../../models/FamilyMember';
+import { TaskList } from '../../../../models/TaskList';
+import { MessageService } from 'primeng/api';
+import { AuthService } from '../../../../services/auth.service';
+import { UserService } from '../../../../services/user.service';
+import { AssignedTaskComponent } from "../assigned-task/assigned-task.component";
 
 @Component({
   selector: 'app-my-tasks',
-  imports: [],
+  imports: [AssignedTaskComponent],
   templateUrl: './my-tasks.component.html',
   styleUrl: './my-tasks.component.less'
 })
-export class MyTasksComponent {
+export class MyTasksComponent implements OnInit  {
+  assignedTasks: TaskList[] = [];
+  user?: FamilyMember;
 
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private messageService: MessageService
+  ) { 
+    this.user = this.userService.member;
+  }
+
+  async ngOnInit(): Promise<void> {
+    if(!this.userService.family) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Oops',
+        detail: "Something went wrong with the authentication: your family wasn't retrieved properly. \nPlease try again."
+      });
+      this.authService.logout();
+    }       
+    if(this.user) {
+      this.assignedTasks = await this.userService.getAssignedTasks();
+    }
+  }
 }
